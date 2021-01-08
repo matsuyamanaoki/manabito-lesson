9ed7d52d8335e039d2bab287f15c0fb16d4f7adb
matsuyamanaoki <mountain.30hope@docomo.ne.jp>
2021-01-08 15:38:48 +0900

    wordpress始まり

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>HOTEL IMPERIAL RESORT TOKYO</title>
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<script src="js/pagetop.js"></script>
+<script src="js/slide.js"></script>
+<?php wp_head(); ?>
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<a href="index.html">
+				<img src="images/logo.png" alt="HOTEL IMPERIAL RESORT TOKYO" width="306" height="49">
+			</a>
+		</div>
+		<nav class="nav-global">
+			<ul>
+				<li>
+					<a href="concept.html">結婚式のコンセプト</a>
+				</li>
+				<li>
+					<a href="plan.html">プランのご案内</a>
+				</li>
+				<li>
+					<a href="fair.html">ブライダルフェア</a>
+				</li>
+				<li>
+					<a href="contact.html">お問い合わせ</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
